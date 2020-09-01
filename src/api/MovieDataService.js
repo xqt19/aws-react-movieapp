4fdcb2d2415bef59ffa351c4588d9aae8916fe37
xqt19 <tan.xiu.qi.11@gmail.com>
2020-09-02 01:10:28 +0800

    completed actors crud

--- src/api/MovieDataService.js
+++ src/api/MovieDataService.js
@@ -1,24 +1,26 @@
 import axios from 'axios'
 
 class MovieDataService {
-    retrieveAllMovies(){
-        // return axios.get(`http://rest-api-xq-dev.eba-bwit5cb9.ap-southeast-1.elasticbeanstalk.com/movies`)
-        return axios.get(`http://localhost:5000/movies`)
+    retrieveAllMovies(view){
+        return axios.get(`http://rest-api-xq-dev.eba-bwit5cb9.ap-southeast-1.elasticbeanstalk.com/movies`)
+        // return axios.get(`http://localhost:5000/index/${view}`, view)
+        // return axios.get(`http://localhost:5000/movies`)
     }
     createNewMovie(movie){
-        // return axios.post(`http://rest-api-xq-dev.eba-bwit5cb9.ap-southeast-1.elasticbeanstalk.com/movies`)
-        return axios.post(`http://localhost:5000/movies`, movie)
+        return axios.post(`http://rest-api-xq-dev.eba-bwit5cb9.ap-southeast-1.elasticbeanstalk.com/movies`, movie)
+        // return axios.post(`http://localhost:5000/movies`, movie)
     }
     deleteMovie(id){
-        // return axios.post(`http://rest-api-xq-dev.eba-bwit5cb9.ap-southeast-1.elasticbeanstalk.com/movies`)
-        return axios.delete(`http://localhost:5000/movies/${id}`)
+        return axios.delete(`http://rest-api-xq-dev.eba-bwit5cb9.ap-southeast-1.elasticbeanstalk.com/movies/${id}`)
+        // return axios.delete(`http://localhost:5000/movies/${id}`)
     }
     updateMovie(movie){
-        // return axios.post(`http://rest-api-xq-dev.eba-bwit5cb9.ap-southeast-1.elasticbeanstalk.com/movies`)
-        return axios.put(`http://localhost:5000/movies`, movie)
+        return axios.put(`http://rest-api-xq-dev.eba-bwit5cb9.ap-southeast-1.elasticbeanstalk.com/movies`,movie)
+        // return axios.put(`http://localhost:5000/movies`, movie)
     }
     getMovie(id){
-        return axios.get(`http://localhost:5000/movies/${id}`)
+        return axios.get(`http://rest-api-xq-dev.eba-bwit5cb9.ap-southeast-1.elasticbeanstalk.com/movies/${id}`)
+        // return axios.get(`http://localhost:5000/movies/${id}`)
     }
 }
 
